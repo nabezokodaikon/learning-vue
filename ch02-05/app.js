db71a3e13e8dc0e99d50f7a024d75b0ac6e78836
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-07-04 00:09:10 +0900

    Add Up to ch2.5

---
+++ ch02-05/app.js
@@ -0,0 +1,26 @@
+const items = [
+  {
+    name: "鉛筆",
+    price: 300,
+    quantity: 0
+  },
+  {
+    name: "ノート",
+    price: 400,
+    quantity: 0
+  },
+  {
+    name: "消しゴム",
+    price: 500,
+    quantity: 0
+  }
+]
+
+const vm = new Vue({
+  el: "#app",
+  data: {
+    items: items
+  }
+});
+
+window.vm = vm;
