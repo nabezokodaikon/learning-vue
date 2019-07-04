9d06bcc82ee8e10deccf2b4a6ad0356165adbfe6
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-07-04 21:17:27 +0900

    Add Up to ch3.2.2

--- ch03-01/app.js
+++ ch03-01/app.js
@@ -1,14 +1,71 @@
-const vm = new Vue({
-  el: "#app",
-  data: {
-    messagePrefix: "Hello"
-  },
-  computed: {
-    message: function() {
-      const timestamp = Date.now();
-      return `${this.messagePrefix}, ${timestamp}`;
-    }
+Vue.component("list-item", {
+  template: "<li>foo {{ contents }}</li>",
+  data: function() {
+    return {
+      contents: "bar"
+    };
   }
 });
 
-window.vm = vm;
+new Vue({
+  el: "#example"
+});
+
+Vue.component("fruits-list-title", {
+  template: "<h1>フルーツ一覧</h1>"
+});
+
+Vue.component("fruits-list-description", {
+  template: "<p>季節の代表的なフルーツの一覧です</p>"
+});
+
+Vue.component("fruits-list-table", {
+  template: `
+  <table>
+    <tr>
+      <th>季節</th>
+      <th>フルーツ</th>
+    </tr>
+    <tr>
+      <td>春</td>
+      <td>いちご</td>
+    </tr>
+    <tr>
+      <td>夏</td>
+      <td>スイカ</td>
+    </tr>
+    <tr>
+      <td>秋</td>
+      <td>ぶどう</td>
+    </tr>
+    <tr>
+      <td>冬</td>
+      <td>みかん</td>
+    </tr>
+  </table>
+  `
+});
+
+new Vue({
+  el: "#fruits-list"
+});
+
+Vue.component("fruits-list", {
+  template: `
+    <div>
+      <fruits-list-title></fruits-list-title>
+    </div>
+  `
+});
+
+new Vue({
+  el: "#main"
+});
+
+const FruitsListTitle = Vue.extend({
+  template: "<h1>フルーツ一覧</h1>"
+});
+
+new FruitsListTitle().$mount("#fruits-list2");
+
+Vue.component("fruits-list-title", FruitsListTitle);
