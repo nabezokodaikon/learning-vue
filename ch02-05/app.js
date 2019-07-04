8bbc6fde928a38b458ef7a70757ca9da88080fcf
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-07-04 09:44:53 +0900

    Add Up to ch2.9

--- ch02-05/app.js
+++ ch02-05/app.js
@@ -37,6 +37,20 @@ const vm = new Vue({
     },
     totalPriceWithTax: function() {
       return Math.floor(this.totalPrice * 1.08);
+    },
+    canBuy: function() {
+      return this.totalPrice >= 1000;
+    },
+    errorMessageClass: function() {
+      return {
+        error: !this.canBuy
+      };
+    },
+    errorMessageStyle: function() {
+      return {
+        border: this.canBuy ? "" : "1px solid red",
+        color: this.canBuy ? "" : "red"
+      };
     }
   }
 });
