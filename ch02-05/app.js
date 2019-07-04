cd62ad6546de40afe25b18e7c5a2af213c9387b5
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-07-04 10:23:32 +0900

    Add Up to ch2.11.2

--- ch02-05/app.js
+++ ch02-05/app.js
@@ -29,6 +29,14 @@ const vm = new Vue({
       return value.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,");
     }
   },
+  methods: {
+    doBuy: function() {
+      alert(`${this.totalPriceWithTax}円のお買い上げ！`);
+      this.items.forEach(item => {
+        item.quantity = 0;
+      });
+    }
+  },
   computed: {
     totalPrice: function() {
       return this.items.reduce((sum, item) => {
@@ -40,11 +48,6 @@ const vm = new Vue({
     },
     canBuy: function() {
       return this.totalPrice >= 1000;
-    },
-    errorMessageClass: function() {
-      return {
-        error: !this.canBuy
-      };
     },
     errorMessageStyle: function() {
       return {
