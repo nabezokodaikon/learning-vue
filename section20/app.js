2f227cd995a12f18513a18c988e6234087a0ead1
y_shimoda <y_shimoda@denkiyagi.jp>
2019-07-10 09:28:07 +0900

    Add section20

---
+++ section20/app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: "#app",
+  data: {
+    list: []
+  },
+  watch: {
+    list: function() {
+      console.log("通常:", this.$refs.list.offsetHeight);
+      this.$nextTick(function() {
+        console.log("nextTick:", this.$refs.list.offsetHeight);
+      })
+    }
+  }
+});
