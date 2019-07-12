3de58a63d4e1fae9a778332114e3ac8b23868a64
y_shimoda <y_shimoda@denkiyagi.jp>
2019-07-12 15:13:54 +0900

    Add Up to section44

--- section38/my-app/src/App.vue
+++ section38/my-app/src/App.vue
@@ -15,7 +15,13 @@ export default {
   },
   created() {
     console.log(this.$store.state.count)
+    console.log(this.$store.getters.max)
+    console.log(this.$store.getters.price('リンゴ'))
     this.$store.commit('increment')
+    this.$store.commit('mutationType', 5);
+    console.log(this.$store.state.count)
+    this.$store.dispatch('actionType', 10);
+    console.log(this.$store.state.count)
   }
 }
 </script>
--- section38/my-app/src/store.js
+++ section38/my-app/src/store.js
@@ -6,11 +6,46 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    list: [
+      { id: 0, name: "リンゴ", price: 100 },
+      { id: 1, name: "バナナ", price: 200 }
+    ],
+    list2: [
+      { id: 0, name: "レモン", price: 300 },
+      { id: 1, name: "メロン", price: 400 }
+    ]
   },
   mutations: {
     increment(state) {
       state.count++;
+    },
+    mutationType(state, payload) {
+      state.count = payload;
+    }
+  },
+  getters: {
+    count(state, getters, rootState, rootGetter) {
+      return state.count;
+    },
+    max(state) {
+      return state.list.reduce((a, b) => {
+        return a.price > b.price ? a.price : b.price;
+      }, 0);
+    },
+    item(state) {
+      return name => state.list.find(el => el.name == name)
+    },
+    name(state, getters) {
+      return id => getters.item(id).name
+    },
+    price(state, getters) {
+      return name => getters.item(name).price
+    }
+  },
+  actions: {
+    actionType({ commit }, payload) {
+      commit('mutationType', payload)
     }
   }
 });
